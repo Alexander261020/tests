8811226eee66263acf67e96fa5b4423d874d1614
Alexander261020 <89227049120@mail.ru>
2022-02-28 17:17:31 +0500

    fixed error with has_role? for nil class

--- app/controllers/levels_controller.rb
+++ app/controllers/levels_controller.rb
@@ -1,6 +1,6 @@
 class LevelsController < ApplicationController
   def show
-    @admin = @user.has_role? :admin
+    @admin = @user.has_role? :admin unless @user.nil?
     @classification = Classification.with_levels.find_by(id: params[:id])
     @level = Level.find_by(id: params[:id])
   end
